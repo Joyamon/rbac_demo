fe47c50d81977bcaaa48950638fccb0db6d4f7e5
zhouyanming <18152007693@163.com>
2025-01-08 11:36:31 +0800

    优化记录用户动作文案

--- accounts/signals.py
+++ accounts/signals.py
@@ -6,7 +6,7 @@ from django.dispatch import Signal
 
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    UserActivity.objects.create(user=user, activity_type='login')
+    UserActivity.objects.create(user=user, activity_type='登录系统')
 
 
 # 自定义信号
@@ -18,19 +18,19 @@ add_role = Signal()
 
 @receiver(user_edited)
 def log_user_edit(sender, user, instance, **kwargs):
-    UserActivity.objects.create(user=user, activity_type=f'edit {instance}')
+    UserActivity.objects.create(user=user, activity_type=f'编辑 {instance}')
 
 
 @receiver(user_deleted)
 def log_user_delete(sender, user, instance, **kwargs):
-    UserActivity.objects.create(user=user, activity_type=f'delete {instance}')
+    UserActivity.objects.create(user=user, activity_type=f'删除 {instance}')
 
 
 @receiver(user_assigned_role)
 def log_user_role_assignment(sender, user, instance, **kwargs):
-    UserActivity.objects.create(user=user, activity_type=f'assign role {instance}')
+    UserActivity.objects.create(user=user, activity_type=f'分配角色给 {instance}')
 
 
 @receiver(add_role)
 def log_add_role(sender, user, instance, **kwargs):
-    UserActivity.objects.create(user=user, activity_type=f'add role {instance}')
+    UserActivity.objects.create(user=user, activity_type=f'添加角色给 {instance}')
